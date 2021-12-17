882951e77ba59ae6e0bc467ae768a4df85649eca
María de las Maravillas Luque Carmona <mmlc0007@red.ujaen.es>
2021-12-17 12:41:11 +0100

    first commit

---
+++ Day1/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: maral
+ *
+ * Created on 17 de diciembre de 2021, 12:39
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+    int contador=0;
+    
+    
+    
+    return 0;
+}
+
