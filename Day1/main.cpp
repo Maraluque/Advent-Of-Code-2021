366896051696a7f3f01e30311ded7568fb4bf7f1
María de las Maravillas Luque Carmona <mmlc0007@red.ujaen.es>
2021-12-17 12:55:10 +0100

    day 1 completed

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -11,6 +11,9 @@
  */
 
 #include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -21,8 +24,26 @@ int main(int argc, char** argv) {
 
     
     int contador=0;
+    ifstream entrada("datos.txt");
+
+    vector<int> vector_datos;
+    string dato="";
+    int num;
+
+    while (getline(entrada, dato)) {
+
+        vector_datos.push_back(stoi(dato));
+
+    }
+    entrada.close();
     
+    for (int i=1; i< vector_datos.size(); i++){
+        if(vector_datos[i]>vector_datos[i-1]){
+            contador++;
+        }
+    }
     
+    cout << "El número total de medidas que han incrementado es de: " << contador << " medidas." << endl;
     
     return 0;
 }
