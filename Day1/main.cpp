1f86b206b769b137a499a0470bcea52a708d8847
María de las Maravillas Luque Carmona <mmlc0007@red.ujaen.es>
2021-12-17 20:02:42 +0100

    day 1 part 2 update in readme.md

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
- */
-
 /* 
  * File:   main.cpp
  * Author: maral
